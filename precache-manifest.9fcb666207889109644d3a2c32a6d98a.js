2a065eda704c006558f9c9c504c634aa65c27604
landroide13 <lazaro02@hotmail.com>
2019-07-15 16:10:16 +1200

    Updates

---
+++ precache-manifest.9fcb666207889109644d3a2c32a6d98a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daf4132f7a47cc241e43cca9e00d56a4",
+    "url": "/Do-It-App/index.html"
+  },
+  {
+    "revision": "47f45c6ecf890486703e",
+    "url": "/Do-It-App/static/css/main.0f2d207b.chunk.css"
+  },
+  {
+    "revision": "eca08089260ff5a39d2b",
+    "url": "/Do-It-App/static/js/2.99769fb4.chunk.js"
+  },
+  {
+    "revision": "47f45c6ecf890486703e",
+    "url": "/Do-It-App/static/js/main.d4fd2e00.chunk.js"
+  },
+  {
+    "revision": "d44e27af89e2d2e04ab2",
+    "url": "/Do-It-App/static/js/runtime~main.b318d67b.js"
+  }
+]);
